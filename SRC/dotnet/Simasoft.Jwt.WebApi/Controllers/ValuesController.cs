a54a4d35a548538cd7f6719b8af42d2341b639ca
Gabriel Simas <gabrielsimas@gmail.com>
2018-11-20 11:59:39 -0200

    Projeto asp.net com mock completo. Ainda nao executado. E nao validado

--- SRC/dotnet/Simasoft.Jwt.WebApi/Controllers/ValuesController.cs
+++ SRC/dotnet/Simasoft.Jwt.WebApi/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Simasoft.Jwt.WebApi.Controllers
@@ -11,6 +12,7 @@ namespace Simasoft.Jwt.WebApi.Controllers
     {
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
@@ -18,6 +20,7 @@ namespace Simasoft.Jwt.WebApi.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
+        [AllowAnonymous] //Sem necessidade de Autorização
         public string Get(int id)
         {
             return "value";
@@ -25,18 +28,21 @@ namespace Simasoft.Jwt.WebApi.Controllers
 
         // POST api/values
         [HttpPost]
+        [Authorize] //Qualquer Role
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
+        [Authorize(Policy = "User")] //Só a Role Admin
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
+        [Authorize(Policy = "User")] //Só a Role User
         public void Delete(int id)
         {
         }
